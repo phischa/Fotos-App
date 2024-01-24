b27abc926f84dac5bd7b62e3f0023ef0df294721
phischa <ph.git@mailbox.org>
2024-01-24 16:02:07 +0100

    first finish

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let images = ['img/beach1.jpg', 'img/sunrise1.jpg', 'img/tree1.jpg', 'img/mountain1.jpg', 'img/fall1.jpg', 'img/winter1.jpg', 'img/lavender1.jpg', 'img/prague1.jpg', 'img/prague2.jpg', 'img/morning1.jpg', 'img/sea1.jpg','img/sea2.jpg', 'img/purple1.jpg', 'img/sunset1.jpg', 'img/sunset2.jpg', 'img/house1.jpg', 'img/iceland1.jpg']
+let images = ['img/beach1.jpg', 'img/sunrise1.jpg', 'img/tree1.jpg', 'img/mountain1.jpg', 'img/fall1.jpg', 'img/winter1.jpg', 'img/lavender1.jpg', 'img/prague1.jpg', 'img/prague2.jpg', 'img/coffee1.jpg', 'img/morning1.jpg', 'img/bird1.jpg', 'img/animal1.jpg', 'img/sea1.jpg', 'img/sea2.jpg', 'img/purple1.jpg', 'img/sunset1.jpg', 'img/sunset2.jpg', 'img/house1.jpg', 'img/iceland1.jpg']
 
 function load() {
     let content = document.getElementById('content');
     content.innerHTML = '';
 
-    for(let i = 0; i < images.length; i++) {
+    for (let i = 0; i < images.length; i++) {
         /* const image = images[i]; */
         document.getElementById('content').innerHTML += /* HTML */ `
             <div>
                 <img class="img-box" onclick="openImage(${i})" src="${images[i]}"> 
             </div>`;
     }
-} 
+}
 
 function openImage(i) {
     document.getElementById('fullscreen').classList.add('img-box-fullscreen');
@@ -30,9 +30,9 @@ function openImageHTML(i) {
 }
 
 function closeImage() {
-        document.getElementById('fullscreen').classList.remove('img-box-fullscreen');
-        fullscreen.innerHTML = '';
-    }
+    document.getElementById('fullscreen').classList.remove('img-box-fullscreen');
+    fullscreen.innerHTML = '';
+}
 
 function nextImage(i) {
     i = (i + 1) % images.length;
